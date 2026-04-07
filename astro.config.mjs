403b8f7fe9c2484a2eb6bcedf64378e2e5e73882
bogdanali-droid <aliciucfamily@gmail.com>
2026-04-07 12:20:27 +0300

    SEO: activate sitemap, add Article schema to BlogPost

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://axait.ro',
   integrations: [
-    tailwind()
-    // sitemap() // Activated after all pages are done
+    tailwind(),
+    sitemap()
   ],
   output: 'static'
 });
